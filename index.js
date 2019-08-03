9b15cd775f6382d710769b555acf1fb55c911fb7
anderson2811 <anderson.andres.guerrero@gmail.com>
2019-08-03 00:52:17 -0500

    Add schema client

--- index.js
+++ index.js
@@ -5,12 +5,39 @@ import { schema } from './schema'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Todo listo')
+  res.send('api is working')
 })
 
+class Client {
+  constructor (id, { name, lastname, email, company }) {
+    this.id = id
+    this.name = name
+    this.lastname = lastname
+    this.email = email
+    this.company = company
+  }
+}
+
+const clientsDB = []
 // Resolver
 const resolver = {
-  hola: () => 'Hola mundo desde graphql'
+  getClient: ({ id }) => {
+    const client = clientsDB.filter(client => client.id === id)
+    console.log(client)
+    if (client.length >= 1) {
+      return client[0]
+    }
+    return null
+  },
+  getClients: () => {
+    return clientsDB
+  },
+  createClient: ({ input }) => {
+    const id = require('crypto').randomBytes(10).toString('hex')
+    const client = new Client(id, input)
+    clientsDB.push(client)
+    return client
+  }
 }
 
 app.use('/api', graphqlHTTP({
@@ -23,5 +50,5 @@ app.use('/api', graphqlHTTP({
 }))
 
 app.listen(8000, () => {
-  console.log('Servidor funcionando puerto 8000')
+  console.log('server running in the port 8000')
 })
--- schema.js
+++ schema.js
@@ -1,7 +1,27 @@
 import { buildSchema } from 'graphql'
 
 export const schema = buildSchema(`
+    type Client {
+        id: ID
+        name: String!
+        lastname: String!
+        company: String
+        email: String
+    }
+
     type Query {
-        hola: String
+        getClient(id: ID!): Client
+        getClients: [Client]
+    }
+
+    input ClientInput {
+        name: String!
+        lastname: String!
+        company: String
+        email: String
+    }
+
+    type Mutation {
+        createClient(input: ClientInput!) : Client
     }
 `)
