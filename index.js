5d3f74643d0456b312c8110d0cbf996835cbca3e
anderson2811 <anderson.andres.guerrero@gmail.com>
2019-08-02 21:01:14 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import graphqlHTTP from 'express-graphql'
+import schema from './schema'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Todo listo')
+})
+
+// Resolver
+const resolver = {
+  hola: () => 'Hola mundo desde graphql'
+}
+
+app.get('/chema graphql', graphqlHTTP({
+  // schema a utilizar
+  schema,
+  // el resolver se pasa como rootValue
+  rootValue: resolver,
+  // Utilizamos graphiql
+  graphiql: true
+}))
+
+app.listen(8000, () => {
+  console.log('Servidor funcionando puerto 8000')
+})
