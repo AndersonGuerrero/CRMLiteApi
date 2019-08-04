17f6851268586ad55bf275b70e3f1c03467ef86f
anderson2811 <anderson.andres.guerrero@gmail.com>
2019-08-03 23:12:54 -0500

    Add connection the mongodb, modify schema and .env

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import express from 'express'
+import 'dotenv/config'
 import graphqlHTTP from 'express-graphql'
-import { schema } from './schema'
+import { schema } from './data/schema'
+
+const port = process.env.PORT || 3000
+const isDev = process.env.NODE_ENV !== 'production'
 
 const app = express()
 
@@ -8,47 +12,13 @@ app.get('/', (req, res) => {
   res.send('api is working')
 })
 
-class Client {
-  constructor (id, { name, lastname, email, company }) {
-    this.id = id
-    this.name = name
-    this.lastname = lastname
-    this.email = email
-    this.company = company
-  }
-}
-
-const clientsDB = []
-// Resolver
-const resolver = {
-  getClient: ({ id }) => {
-    const client = clientsDB.filter(client => client.id === id)
-    console.log(client)
-    if (client.length >= 1) {
-      return client[0]
-    }
-    return null
-  },
-  getClients: () => {
-    return clientsDB
-  },
-  createClient: ({ input }) => {
-    const id = require('crypto').randomBytes(10).toString('hex')
-    const client = new Client(id, input)
-    clientsDB.push(client)
-    return client
-  }
-}
-
 app.use('/api', graphqlHTTP({
   // schema a utilizar
   schema,
-  // el resolver se pasa como rootValue
-  rootValue: resolver,
   // Utilizamos graphiql
-  graphiql: true
+  graphiql: isDev
 }))
 
-app.listen(8000, () => {
-  console.log('server running in the port 8000')
+app.listen(port, () => {
+  console.log(`server running in the port ${port}`)
 })
