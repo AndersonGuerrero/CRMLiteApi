14eaf070394bfee0b46eeb964078def7a62f9b60
anderson2811 <anderson.andres.guerrero@gmail.com>
2019-08-05 02:18:31 -0500

    fixs

--- src/schema.js
+++ src/schema.js
@@ -1,3 +1,4 @@
 import { importSchema } from 'graphql-import'
+import path from 'path'
 
-export const typeDefs = importSchema('src/schema.graphql')
+export const typeDefs = importSchema(path.join(__dirname, '/schema.graphql'))
