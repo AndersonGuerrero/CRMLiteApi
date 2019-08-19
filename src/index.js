e2ae5bd4774347506f66fa7d721fed1318eead44
anderson2811 <anderson.andres.guerrero@gmail.com>
2019-08-19 02:05:06 -0500

    adding authentication

--- src/index.js
+++ src/index.js
@@ -2,12 +2,32 @@ import express from 'express'
 import { ApolloServer } from 'apollo-server-express'
 import typeDefs from './schema/schema.graphql'
 import { resolvers } from './resolvers'
+import jwt from 'jsonwebtoken'
 
 const port = process.env.PORT || 3000
 
 const app = express()
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({
+	typeDefs,
+  resolvers,
+  context: async({ req})=>{
+    //Obtener token del cliente
+    const token = req.headers['authorization']
+    if ( token !== 'null'){
+      try{
+        const currentUser = await jwt.verify(token, process.env.SECRET_KEY)
+        // Agregado user actual al request
+        req.currentUser = currentUser
+        return {
+          currentUser
+        }
+      }catch(error){
+        console.error(error)
+      }
+    }
+  } 
+})
 
 server.applyMiddleware({ app })
 
--- src/resolvers/usersResolver.js
+++ src/resolvers/usersResolver.js
@@ -1,8 +1,23 @@
 import { Users } from '../db'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 // Resolver
+const createToken = (user, secretKey, expiresIn) => {
+  const { username } = user
+  return jwt.sign({ username }, secretKey, { expiresIn }) 
+}
+
 export const usersResolver = {
   Query: {
+    getAuthenticatedUser: (root, arg, { currentUser }) => {
+      if(!currentUser){
+        return null
+      }
+      // Opteniendo el usuario actual del request
+      const user = Users.findOne({username: currentUser.username})
+      return user
+    },
     getUsers: (root, { limit, offset }) => {
       return new Promise((resolve, reject) => {
         Users.find({}, null, { limit, skip: offset }, (error, users) => {
@@ -32,6 +47,33 @@ export const usersResolver = {
         response = {
           error: false,
           message: 'Usuario registrado con Exito.'
+        }
+      }
+      return response
+    },
+    authentication: async (root, { input }) => {
+      const { password, username } = input
+      let response = { error: false, message: '', token: '' }
+      const userExist = await Users.findOne({ username })
+      if ( !userExist ) {
+        response = {
+          error: true,
+          message: 'El Usuario no Existe!'
+        }
+      } else { 
+        const passwordCheck = await bcrypt.compare(password, userExist.password)
+        if (passwordCheck) {
+          const newToken = createToken(userExist, process.env.SECRET_KEY, '1hr')
+          response = {
+            error: false,
+            message: 'Bienvenido!',
+            token: newToken
+          }
+        }else{
+          response = {
+            error: true,
+            message: 'Contaseña Incorrecta!'
+          }
         }
       }
       return response
